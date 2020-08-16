223994468cdc760c40b2ddad49de1eb50b63113f
Raghad Alrasheed <raghad@MacBook-Pro-alkhas-b-Raghad.local>
2020-08-16 16:13:12 +0300

    operation code

--- calculator.py
+++ calculator.py
@@ -1,8 +1,20 @@
 
 def main():
-	#write your code here
-	pass
-	
+      number1 = input(" Enter the first number :  ")
+      n1 = int(number1)
+      number2 = input(" Enter the second number : ")
+      n2 = int(number2)
+      operation = input(" Choose the operation (+, -, /, *) : ")
+      answer = 0
+      if operation == "+":
+        answer =  n1+n2
+      elif operation == "-":
+        answer = n1-n2
+      elif operation == "/":
+        answer = n1/n2
+      elif operation == "*":
+        answer = n1*n2
+      print("The answer is " + str(answer))
 
 
 
