de1beb4a7813f46df89a03465104fb90d890e000
Raghad Alrasheed <raghad@MacBook-Pro-alkhas-b-Raghad.local>
2020-08-18 09:57:55 +0300

    code fixed

--- calculator.py
+++ calculator.py
@@ -1,21 +1,29 @@
 
-def main():
-      number1 = input(" Enter the first number :  ")
-      n1 = int(number1)
-      number2 = input(" Enter the second number : ")
-      n2 = int(number2)
-      operation = input(" Choose the operation (+, -, /, *) : ")
-      answer = 0
-      if operation == "+":
-        answer =  n1+n2
-      elif operation == "-":
-        answer = n1-n2
-      elif operation == "/":
-        answer = n1/n2
-      elif operation == "*":
-        answer = n1*n2
-      print("The answer is " + str(answer))
 
+def calc(number1,number2,operation):
+   if (number1.isnumeric() and number2.isnumeric())==True:
+       if operation=='+':
+           answer=int(number1)+int(number2)
+           print("The answer is %d "%(answer))
+       elif operation=='-':
+           answer=int(number1)-int(number2)
+           print("The answer is %d "%(answer))
+       elif operation=='/':
+           answer=int(number1)/int(number2)
+           print("The answer is %d "%(answer))
+       elif operation=='':
+           answer=int(number1)*int(number2)
+           print("The answer is %d "%(answer))
+       else:
+            print("operation is invalid")
+   else:
+       print("the entered numbers are invalid")
+def main():
+
+  number1=input("Enter the first number:")
+  number2=input("Enter the second number: ")
+  operation=input("Choose the operation (+, -, /, ): ")
+  calc(number1,number2,operation)
 
 
 if __name__ == '__main__':
